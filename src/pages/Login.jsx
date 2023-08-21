826f6d1d600cbc6404005948673432b368b652a0
prop07 <rijalmanoj07@gmail.com>
2023-08-21 15:14:28 +0545

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,8 @@ import {
 } from "firebase/auth";
 import { auth, googleProvider } from "../config/firebase";
 import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 
 const Login = () => {
   const redirect = useNavigate();
@@ -15,26 +17,33 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  console.log(auth?.currentUser?.email);
-  console.log(auth?.currentUser?.photoURL);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  // console.log(auth?.currentUser?.email);
+  // console.log(auth?.currentUser?.photoURL);
 
   const handleTogglePassword = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
 
   const sinIn = async () => {
+    
     try {
-      console.log(email, password);
-      await signInWithEmailAndPassword(auth, email, password);
-      redirect("/");
+      console.log(email + password);
+      // await signInWithEmailAndPassword(auth, email, password);
+      // redirect("/");
     } catch (err) {
-      console.log(err);
+      console.log(JSON.stringify(err));
     }
   };
 
   const sinInWithGoogle = async () => {
     try {
-      console.log(email, password);
+
       await signInWithPopup(auth, googleProvider);
       redirect("/");
     } catch (err) {
@@ -67,19 +76,24 @@ const Login = () => {
               If you are already a member, easily log in.
             </p>
 
-            <form action="" className="flex flex-col gap-4">
+            <form onSubmit={handleSubmit(sinIn)} className="flex flex-col gap-4">
               <input
                 className="p-2 mt-8 rounded-xl border"
                 onChange={(e) => setEmail(e.target.value)}
-                type="email"
+                {...register("email", {
+                  required: "Email required !"
+                })}
+                type="text"
                 name="email"
                 placeholder="Email"
               />
+              <p className="text-red-500 text-sm pl-4">{errors.email?.message}</p>
               <div className="relative">
-                <span className="flex items-center gap-1">
+                <span className="flex items-center gap-2">
                   <input
                     className="p-2 rounded-xl border w-full"
                     onChange={(e) => setPassword(e.target.value)}
+                    {...register("password", { required: "Password required !" })}
                     type={showPassword ? "text" : "password"}
                     placeholder="Password"
                   />
@@ -97,6 +111,7 @@ const Login = () => {
                     />
                   )}
                 </span>
+                <p className="mt-2 text-red-500 text-sm pl-4">{errors.password?.message}</p>
               </div>
               <button
                 className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300"
@@ -105,13 +120,11 @@ const Login = () => {
                 Login
               </button>
             </form>
-
             <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
               <hr className="border-gray-400" />
               <p className="text-center text-sm">OR</p>
               <hr className="border-gray-400" />
             </div>
-
             <button
               onClick={sinInWithGoogle}
               className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]"
@@ -141,15 +154,12 @@ const Login = () => {
               </svg>
               Login with Google
             </button>
-
             <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
               <a href="#">Forgot your password?</a>
             </div>
-
             <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
-              <p>Don't have an account?</p>
+              <p>Don&apos;t have an account?</p>
               <Link to={"/register"}>
-                {" "}
                 <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
                   Register
                 </button>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,7 +5,7 @@ import {
     signOut,
 } from "firebase/auth";
 import { auth } from "../config/firebase";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 let rendercount = 0;
@@ -21,10 +21,12 @@ const Register = () => {
         formState: { errors }
     } = useForm();
     console.log(errors)
-    const redirect = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errorResponse, setErrorResponse] = useState(null
+    );
     const [showPassword, setShowPassword] = useState(false);
+    const [registerState, setRegisterState] = useState(false);
 
     // console.log(auth?.currentUser?.email);
     // console.log(auth?.currentUser?.photoURL);
@@ -34,12 +36,19 @@ const Register = () => {
     };
 
     const sinIn = async () => {
+        setErrorResponse(null);
+        setRegisterState(false);
         try {
             console.log(email + password);
             await createUserWithEmailAndPassword(auth, email, password);
-            redirect("/login");
+            setRegisterState(true);
+            // redirect("/login");
         } catch (err) {
-            console.log(err);
+
+            setErrorResponse({
+                code: err.customData._tokenResponse.error.code,
+                message: err.customData._tokenResponse.error.message
+            })
         }
     };
 
@@ -55,6 +64,7 @@ const Register = () => {
     };
     return (
         <div>
+            <button onClick={logout}>Logout</button>
             <center>{rendercount}</center>
             <section className="bg-gray-50 min-h-screen flex items-center justify-center">
                 <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
@@ -63,32 +73,55 @@ const Register = () => {
                         <p className="text-xs mt-4 text-[#002D74]">It’s quick and easy.</p>
                         <form
                             onSubmit={handleSubmit(sinIn)}
-                            className="flex flex-col gap-4 border-b border-[#002D74] py-4 text-[#002D74]"
+                            className="flex flex-col gap-4"
                         >
                             <input
                                 className="p-2 mt-8 rounded-xl border"
-                                {...register("email", { required: "Email required !" })}
+                                {...register("email", {
+                                    required: "Email required !",
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                                        message: 'Invalid email format !',
+                                    }
+                                })}
                                 onChange={(e) => setEmail(e.target.value)}
-                                type="email"
+                                type="text"
                                 placeholder="Email"
                             />
-                            <p className="text-sm text-red-500">{errors.email?.message}</p>
+                            <p className="text-red-500 text-sm pl-4">{errors.email?.message}</p>
                             <div className="relative">
                                 <span className="flex items-center gap-2"> <input
                                     className="p-2 rounded-xl border w-full"
-                                    {...register("password", { required: "Password required !", minLength: { value: 8, message: "Password min length is 8 !" } })}
+                                    {...register("password", { required: "Password required !", minLength: { value: 8, message: "Min length is 8 !" } })}
                                     onChange={(e) => setPassword(e.target.value)}
                                     type={showPassword ? "text" : "password"}
                                     placeholder="Password"
                                 />
                                     {showPassword ? <FaEyeSlash className="cursor-pointer hover:text-cyan-600 " size={25} onClick={handleTogglePassword} /> : <FaEye className="cursor-pointer hover:text-cyan-600 " size={25} onClick={handleTogglePassword} />}   </span>
-                                <p className="mt-4 text-sm text-red-500">{errors.password?.message}</p>
+                                <p className="mt-2 text-red-500 text-sm pl-4">{errors.password?.message}</p>
                             </div>
+                            {registerState ? <div className="flex items-center rounded-lg p-4 mb-4 text-green-800  bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800" role="alert">
+                                <svg className="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                                </svg>
+                                <p className="ml-3 text-sm">Account Registered successfully.</p>
+                            </div> : null}
+                            {errorResponse ? <div className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                                <svg className="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                                </svg>
+                                <p className="mi-3 text-sm">{errorResponse.message}. Error Code: {errorResponse.code}</p>
+                            </div> : null}
                             <button
                                 className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300"
                                 type="submit"
                             >Register</button>
                         </form>
+                        <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
+                            <hr className="border-gray-400" />
+                            <p className="text-center text-sm">OR</p>
+                            <hr className="border-gray-400" />
+                        </div>
                         <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
                             <p>Have an account?</p>
                             <Link to={"/login"}>
